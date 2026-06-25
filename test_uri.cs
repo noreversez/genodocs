0d800088838fc4a8fa018070626692a816445e0c
Geno <nozzzth@gmail.com>
2026-06-25 08:10:40 +0700

    chore: remove build artifacts and add .gitignore

--- test_uri.cs
+++
@@ -1,7 +0,0 @@
-using System;
-class Program {
-    static void Main() {
-        string q = "?name=%E0%B8%84%E0%B8%94%E0%B8%B5";
-        Console.WriteLine(Uri.UnescapeDataString(q.Replace(""?name="", """")));
-    }
-}
